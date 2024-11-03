318cfaa8004603d9bbb1cdc4630352d9a002901b
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-11-03 13:20:35 +0100

    chore: refactor `command`

--- shadcn/command.tsx
+++ shadcn/command.tsx
@@ -7,13 +7,11 @@ import { type ComponentPropsWithoutRef, type ElementRef, type HTMLAttributes, fo
 import { Dialog, DialogContent } from "shadcn/dialog"
 import type { CommandDialogProperties } from "types/components"
 
-const Command = forwardRef<ElementRef<typeof CommandPrimitive>, ComponentPropsWithoutRef<typeof CommandPrimitive>>(({ className, ...properties }, reference) => (
+export const Command = forwardRef<ElementRef<typeof CommandPrimitive>, ComponentPropsWithoutRef<typeof CommandPrimitive>>(({ className, ...properties }, reference) => (
 	<CommandPrimitive ref={reference} className={mergeClassNames("flex h-full w-full flex-col overflow-hidden rounded-md bg-popover text-popover-foreground", className)} {...properties} />
 ))
 
-Command.displayName = CommandPrimitive.displayName
-
-const CommandDialog = ({ children, ...properties }: CommandDialogProperties) => {
+export const CommandDialog = ({ children, ...properties }: CommandDialogProperties) => {
 	return (
 		<Dialog {...properties}>
 			<DialogContent className="overflow-hidden p-0 shadow-lg">
@@ -25,7 +23,7 @@ const CommandDialog = ({ children, ...properties }: CommandDialogProperties) => 
 	)
 }
 
-const CommandInput = forwardRef<ElementRef<typeof CommandPrimitive.Input>, ComponentPropsWithoutRef<typeof CommandPrimitive.Input>>(({ className, ...properties }, reference) => (
+export const CommandInput = forwardRef<ElementRef<typeof CommandPrimitive.Input>, ComponentPropsWithoutRef<typeof CommandPrimitive.Input>>(({ className, ...properties }, reference) => (
 	<div className="flex items-center border-b px-3" cmdk-input-wrapper="">
 		<Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
 		<CommandPrimitive.Input
@@ -39,21 +37,15 @@ const CommandInput = forwardRef<ElementRef<typeof CommandPrimitive.Input>, Compo
 	</div>
 ))
 
-CommandInput.displayName = CommandPrimitive.Input.displayName
-
-const CommandList = forwardRef<ElementRef<typeof CommandPrimitive.List>, ComponentPropsWithoutRef<typeof CommandPrimitive.List>>(({ className, ...properties }, reference) => (
+export const CommandList = forwardRef<ElementRef<typeof CommandPrimitive.List>, ComponentPropsWithoutRef<typeof CommandPrimitive.List>>(({ className, ...properties }, reference) => (
 	<CommandPrimitive.List ref={reference} className={mergeClassNames("max-h-[300px] overflow-y-auto overflow-x-hidden", className)} {...properties} />
 ))
 
-CommandList.displayName = CommandPrimitive.List.displayName
-
-const CommandEmpty = forwardRef<ElementRef<typeof CommandPrimitive.Empty>, ComponentPropsWithoutRef<typeof CommandPrimitive.Empty>>((properties, reference) => (
+export const CommandEmpty = forwardRef<ElementRef<typeof CommandPrimitive.Empty>, ComponentPropsWithoutRef<typeof CommandPrimitive.Empty>>((properties, reference) => (
 	<CommandPrimitive.Empty ref={reference} className="py-6 text-center text-sm" {...properties} />
 ))
 
-CommandEmpty.displayName = CommandPrimitive.Empty.displayName
-
-const CommandGroup = forwardRef<ElementRef<typeof CommandPrimitive.Group>, ComponentPropsWithoutRef<typeof CommandPrimitive.Group>>(({ className, ...properties }, reference) => (
+export const CommandGroup = forwardRef<ElementRef<typeof CommandPrimitive.Group>, ComponentPropsWithoutRef<typeof CommandPrimitive.Group>>(({ className, ...properties }, reference) => (
 	<CommandPrimitive.Group
 		ref={reference}
 		className={mergeClassNames(
@@ -64,31 +56,18 @@ const CommandGroup = forwardRef<ElementRef<typeof CommandPrimitive.Group>, Compo
 	/>
 ))
 
-CommandGroup.displayName = CommandPrimitive.Group.displayName
+export const CommandSeparator = forwardRef<ElementRef<typeof CommandPrimitive.Separator>, ComponentPropsWithoutRef<typeof CommandPrimitive.Separator>>(
+	({ className, ...properties }, reference) => <CommandPrimitive.Separator ref={reference} className={mergeClassNames("-mx-1 h-px bg-border", className)} {...properties} />,
+)
 
-const CommandSeparator = forwardRef<ElementRef<typeof CommandPrimitive.Separator>, ComponentPropsWithoutRef<typeof CommandPrimitive.Separator>>(({ className, ...properties }, reference) => (
-	<CommandPrimitive.Separator ref={reference} className={mergeClassNames("-mx-1 h-px bg-border", className)} {...properties} />
-))
-
-CommandSeparator.displayName = CommandPrimitive.Separator.displayName
-
-const CommandItem = forwardRef<ElementRef<typeof CommandPrimitive.Item>, ComponentPropsWithoutRef<typeof CommandPrimitive.Item>>(({ className, ...properties }, reference) => (
+export const CommandItem = forwardRef<ElementRef<typeof CommandPrimitive.Item>, ComponentPropsWithoutRef<typeof CommandPrimitive.Item>>(({ className, ...properties }, reference) => (
 	<CommandPrimitive.Item
 		ref={reference}
-		className={mergeClassNames(
-			"relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none data-[disabled=true]:pointer-events-none data-[selected='true']:bg-accent data-[selected=true]:text-accent-foreground data-[disabled=true]:opacity-50",
-			className,
-		)}
+		className={mergeClassNames("relative flex cursor-default select-none items-center rounded-md p-2 text-sm outline-none", className)}
 		{...properties}
 	/>
 ))
 
-CommandItem.displayName = CommandPrimitive.Item.displayName
-
-const CommandShortcut = ({ className, ...properties }: HTMLAttributes<HTMLSpanElement>) => {
+export const CommandShortcut = ({ className, ...properties }: HTMLAttributes<HTMLSpanElement>) => {
 	return <span className={mergeClassNames("ml-auto text-xs tracking-widest text-muted-foreground", className)} {...properties} />
 }
-
-CommandShortcut.displayName = "CommandShortcut"
-
-export { Command, CommandDialog, CommandInput, CommandList, CommandEmpty, CommandGroup, CommandItem, CommandShortcut, CommandSeparator }
