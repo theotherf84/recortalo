dec73631fbdb00d8bfff17926f0aaae0f939d3c9
theotherf84 <176298840+theotherf84@users.noreply.github.com>
2024-11-03 13:20:08 +0100

    feat: add `p` and `small` components to `shadcn`

--- shadcn/typography.tsx
+++ shadcn/typography.tsx
@@ -17,6 +17,14 @@ export const Heading4 = forwardRef<HTMLHeadingElement, HTMLAttributes<HTMLHeadin
 	<h4 className={mergeClassNames("scroll-m-20 text-xl font-semibold tracking-tight", className)} ref={reference} {...properties} />
 ))
 
+export const Muted = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLParagraphElement>>(({ className, ...properties }, reference) => (
+	<p className={mergeClassNames("text-sm text-muted-foreground", className)} ref={reference} {...properties} />
+))
+
 export const Paragraph = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLParagraphElement>>(({ className, ...properties }, reference) => (
 	<p className={mergeClassNames("leading-6", className)} ref={reference} {...properties} />
+))
+
+export const Small = forwardRef<HTMLSpanElement, HTMLAttributes<HTMLSpanElement>>(({ className, ...properties }, reference) => (
+	<small className={mergeClassNames("text-sm font-medium leading-none", className)} ref={reference} {...properties} />
 ))
